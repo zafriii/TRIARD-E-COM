9deea2381debfbf8bad5bf691de5b03f74a11e35
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-04-19 23:23:43 +0600

    Update MyImage.jsx

--- src/components/Single Product/MyImage.jsx
+++ src/components/Single Product/MyImage.jsx
@@ -12,27 +12,7 @@ import ReactImageMagnify from 'react-image-magnify';
     <>
 
     <div className="myimage-container">
-
-    {/* <div className="four-images">
-        {imgs.map((curElm, index) => {
-          return (
-            <figure>
-              <img
-                src={curElm.url}
-                alt={curElm.filename}
-                className="box-image--style"
-                key={index}
-                onClick={() => setMainImage(curElm)}
-              />
-            </figure>
-          );
-        })}
-      </div> */}
-
-      {/* <div className="main-screen">
-        <img src={mainImage.url} alt={mainImage.filename} className="image"/>
-      </div> */}
-
+     
       <ReactImageMagnify {...{
     smallImage: {
         alt: mainImage.filename,
